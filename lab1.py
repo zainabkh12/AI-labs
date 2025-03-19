40454e48bb7e4eb6d73db4f0b431d3fdc0a6756e
Zainab BCS22049 <khzainab311@gmail.com>
2025-03-19 16:12:29 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,87 @@
+print("My Name Is Alishba." , "And i m a girl.")
+
+# Comments in Python
+# This is a single-line comment
+print("Hello, World!")  # This line prints "Hello, World!"
+
+# Taking input from the user
+
+x=("My name is Alishba")
+print(x)
+
+
+# Multiple statements on a single line
+print("My name is Alishaba");print("And i m a girl")
+
+# Indentation Example
+a=5
+if(a>=1):
+ print("You can order now")
+
+# Data Types Example
+x = 10       # Integer
+y = 10.5     # Float
+z = "Hello"  # String
+
+print(type(x))  # Output: <class 'int'>
+print(type(y))  # Output: <class 'float'>
+print(type(z))  # Output: <class 'str'>
+
+
+#Complex Numbers
+k=3+3j
+print(type(k))
+
+
+# 2. Boolean
+x=True
+print(type(x))
+y=False
+print(type(y))
+
+# 3. Strings
+text = "Hello, Python!"
+print("Original String:", text)
+
+# Accessing elements
+print("First character:", text[0])  # H
+print("Last character:", text[-1])  # !
+
+# String Slicing
+print("First 5 characters:", text[:5])  # Hello
+print("Last 6 characters:", text[-6:])  # Python!
+
+
+#Special characters in strings print("The is a backslash (\\) mark.") 
+print("This is tab \t key") 
+print("These are \"double quotes\"") 
+print ("These are \'single quotes\'") 
+print("This is a new line\nNew line") 
+
+# List Example
+# Creating a list
+fruits = ["Apple", "Banana", "Cherry", "Mango"]
+print("Complete List:", fruits)  # Output: ['Apple', 'Banana', 'Cherry', 'Mango']
+
+# Accessing elements
+print("First fruit:", fruits[0])  # Output: Apple
+print("Last fruit:", fruits[-1])  # Output: Mango
+
+# Slicing a list
+print("First two fruits:", fruits[:2])  # Output: ['Apple', 'Banana']
+
+#  using negative indices
+print("Last two fruits (-2:):", fruits[-2:])  # Output: ['Cherry', 'Mango']
+print("Middle two fruits (-3:-1):", fruits[-3:-1])  # Output: ['Banana', 'Cherry']
+
+# Conditional Statements Example
+a = 20
+b = 15
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than or equal to a")
+
+# Output:
+# a is greater than b
+
